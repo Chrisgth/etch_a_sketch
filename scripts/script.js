71a683cb1cb4fa9977a1392b2219e226f9e8f8b7
chrisgth <ksemkus00@gmail.com>
2022-03-14 14:27:54 +0200

    Created button and added incomplete functionality

--- scripts/script.js
+++ scripts/script.js
@@ -10,7 +10,23 @@ for(a=0; a < 16; a++){
     for(b=0; b < 16; b++){
         let gridRowSquare = document.createElement('div');
         gridRowSquare.classList.add('gridRowSquare');
-        gridRowSquare.addEventListener('mouseover', () => {gridRowSquare.classList.toggle('toggleblack')})
+        gridRowSquare.addEventListener('mouseover', () => {gridRowSquare.classList.add('toggleblack')})
         gridColumn.appendChild(gridRowSquare);
     }
 }
+const resetButton = document.querySelector('#reset')
+resetButton.addEventListener('click', () => {reset()})
+function reset(){
+    container.innerHTML = '';
+    let numPerSide = prompt('how many');
+    numPerSide = Number(numPerSide);
+    console.log(numPerSide);
+}
+// Add a button to the top of the screen which will clear the current grid and send the user a popup asking
+// for the number of squares per side for the new grid. Once entered, the new grid should be generated 
+// in the same total space as before (e.g. 960px wide) so that you’ve got a new sketch pad. 
+// Tip: Set the limit for the user input to a maximum of 100. 
+// A larger number of squares results in more computer resources being used, potentially causing delays, freezing, or crashing that we want to prevent.
+// Research button tags in HTML and how you can make a JavaScript function run when one is clicked.
+// Also check out prompts.
+// You should be able to enter 64 and have a brand new 64x64 grid pop up without changing the total amount of pixels used.
